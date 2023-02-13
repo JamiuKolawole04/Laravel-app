d81572ff8f94c814e9ba9e08585ede9714fddd17
JamiuKolawole04 <Jakko4real@gmail.com>
2023-02-13 08:19:06 +0100

    chore: [migration]: adding columns to task table and migrating

--- database/migrations/2023_02_09_020534_create_tasks_table.php
+++ database/migrations/2023_02_09_020534_create_tasks_table.php
@@ -15,6 +15,18 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            // user id for created tasks
+            $table->unsignedBigInteger("user_id");
+            $table->string("name");
+            $table->string("description");
+            $table->string("priority")->default("medium");
+
+            // definging foreign constarints that must be equal to the user_id column above on users table referencing the id and deleteing in a 
+            // cascading fashion
+            $table->foreign("user_id")
+              ->references("id")
+              ->on("users")
+              ->onDelete("cascade");
             $table->timestamps();
         });
     }
